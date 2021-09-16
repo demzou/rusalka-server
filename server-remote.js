03442ebbe340cae8e6c672a0c0c8a0cdb757278f
demzou <clemence.debaig@gmail.com>
2021-09-16 15:20:37 +0100

    Trying to push server-remote again

--- server-remote.js
+++ server-remote.js
@@ -4,7 +4,7 @@
 //const cors = require ('cors');
 const express = require('express');   //Dependencies
 const app = express();
- const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 app.use(express.static(`${__dirname}/public`));
 //app.use(cors());
 
